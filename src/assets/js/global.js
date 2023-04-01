ea2996cd46d22ddd4a16adc3df0adac85616fd71
VietThang269 <114757489+VietThang269@users.noreply.github.com>
2023-04-01 21:54:42 +0700

    - Update SignUp, Sign In form

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -4,6 +4,12 @@ const hamburger = document.querySelector(".hamburger_nav");
 const navLinkMobile = document.querySelector(".header_navbar_mobile");
 const overlay = document.querySelector(".overlay");
 const closeNavbarMobile = document.querySelector(".close_navbar_mobile");
+const btnSignin = document.querySelector(".btn_signIn");
+const signIn = document.getElementById("sign_in");
+const signUp = document.getElementById("sign_up");
+
+const navSignUp = document.querySelector(".nav_sign_up");
+const navSignIn = document.querySelector(".nav_sign_in");
 
 function hanldeCloseNavMobile() {
   navLinkMobile.classList.remove("active");
@@ -11,6 +17,8 @@ function hanldeCloseNavMobile() {
 
 function hanldeClosePopUp() {
   // navLinkMobile.classList.remove("active");
+  signIn.classList.contains("active") && signIn.classList.remove("active");
+  signUp.classList.contains("active") && signUp.classList.remove("active");
 }
 
 window.onscroll = function () {
@@ -34,4 +42,19 @@ closeNavbarMobile.addEventListener("click", function () {
   hanldeCloseNavMobile();
   hanldeClosePopUp();
   overlay.classList.remove("active");
+});
+
+btnSignin.addEventListener("click", function () {
+  signIn.classList.add("active");
+  overlay.classList.add("active");
+});
+
+navSignUp.addEventListener("click", function () {
+  signIn.classList.remove("active");
+  signUp.classList.add("active");
+});
+
+navSignIn.addEventListener("click", function () {
+  signIn.classList.add("active");
+  signUp.classList.remove("active");
 });
