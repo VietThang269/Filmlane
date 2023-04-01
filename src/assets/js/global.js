0694d63697ccdbdb9c86452f5994f8341cee7cd1
VietThang269 <114757489+VietThang269@users.noreply.github.com>
2023-04-01 19:33:19 +0700

    - Responsive Homepage, Header, Footer

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,8 +1,37 @@
 // Header transition
 const header = document.getElementById("header");
+const hamburger = document.querySelector(".hamburger_nav");
+const navLinkMobile = document.querySelector(".header_navbar_mobile");
+const overlay = document.querySelector(".overlay");
+const closeNavbarMobile = document.querySelector(".close_navbar_mobile");
+
+function hanldeCloseNavMobile() {
+  navLinkMobile.classList.remove("active");
+}
+
+function hanldeClosePopUp() {
+  // navLinkMobile.classList.remove("active");
+}
 
 window.onscroll = function () {
   window.scrollY >= 10
     ? header.classList.add("header_active")
     : header.classList.remove("header_active");
 };
+
+hamburger.addEventListener("click", function () {
+  navLinkMobile.classList.add("active");
+  overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", function () {
+  hanldeCloseNavMobile();
+  hanldeClosePopUp();
+  overlay.classList.remove("active");
+});
+
+closeNavbarMobile.addEventListener("click", function () {
+  hanldeCloseNavMobile();
+  hanldeClosePopUp();
+  overlay.classList.remove("active");
+});
